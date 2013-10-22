dc7a8a10af68a7286249a0d7acc13d7c56104a3b
Kaung Htet Zaw <emoosx@gmail.com>
2013-10-22 11:10:31 +0800

    That's it for P2

--- P2/proxy.java
+++ P2/proxy.java
@@ -4,11 +4,16 @@ import java.util.*;
 
 class proxy {
   
+  public static Map<String, String> fs = new HashMap<String, String>();
+  public static Map<String, String> fr = new HashMap<String, String>();
+  
   public static void main(String[] args) throws IOException {
 
     int port = Integer.parseInt(args[0]);
     String fileSub = args[1];
     String fileRedirect = args[2];
+
+    createFS(fileSub); createFR(fileRedirect);
 
     ServerSocket serverSocket = new ServerSocket(port); 
     while(true) {
@@ -25,6 +30,7 @@ class proxy {
 
       clientSocket = serverSocket.accept();
     
+      
       OutputStream toClient = clientSocket.getOutputStream();
       InputStream fromClient = clientSocket.getInputStream();
 
@@ -36,19 +42,22 @@ class proxy {
         clientBuffer.write(request, 0, bytesRead);    // get the body message
         if(endOfRequest(request)) break;
       }
-      clientBuffer.flush();
       request = clientBuffer.toByteArray();
+      clientBuffer.flush();
 
       InputStream inputStream = new ByteArrayInputStream(request);
       BufferedReader readerFromClient = new BufferedReader(new InputStreamReader(inputStream));
 
       String[] temp;
-      String line, hostname = null;
+      String line, hostname = null, contentType = null;
       while((line = readerFromClient.readLine()) != null) {
         temp = line.split(" ");
         if(temp[0].equals("Host:"))
           hostname = temp[1];
       }
+
+      hostname = checkRedirect(hostname);
+        //System.out.println(hostname);
 
       // STEP 2 :: Forward the HTTP Requests to server host
       server = new Socket(hostname, 80);
@@ -57,7 +66,18 @@ class proxy {
 
 
       // STEP 3 :: get the repsonse data from server
-      DataInputStream fromServer = new DataInputStream(server.getInputStream());
+      InputStream tempInputStream = server.getInputStream();
+      BufferedReader in = new BufferedReader(new InputStreamReader(tempInputStream));      
+      String inputLine;
+      while ((inputLine = in.readLine()) != null) {
+        temp = inputLine.split(" ");
+        if(temp[0].equals("Content-Type:"))
+          contentType = temp[1];
+
+      }
+      in.close();
+      DataInputStream fromServer = new DataInputStream(tempInputStream);
+      
       byte[] data = new byte[40960];
       int i;
       while((i = fromServer.read(data, 0, data.length)) != -1) {
@@ -87,4 +107,53 @@ class proxy {
     }
     return false;
   }
+
+  public static String checkRedirect(String hostname) {
+    if(fr.containsKey(hostname)) {
+      String answer = fr.get(hostname);
+      return answer.substring(7);
+    }
+    return hostname;
+  }
+
+  public static void createFS(String fileSub) {
+    BufferedReader in = null;
+    try {
+      in = new BufferedReader(new FileReader(fileSub));
+      String [] temp;
+      String str;
+      while((str = in.readLine()) != null) {
+        temp = str.split(" ");
+        fs.put(temp[0], temp[1]);
+      }
+    } catch(IOException e) {
+      System.err.println("IOException");
+    } finally {
+      try{
+      if(in != null) in.close();
+      } catch(IOException e) {};
+    }
+  
+  }
+
+  public static void createFR(String fileRedirect) {
+    BufferedReader in = null;
+    try {
+      in = new BufferedReader(new FileReader(fileRedirect));
+      String str;
+      String [] temp;
+      while((str = in.readLine()) != null) {
+        temp = str.split(" ");
+        fr.put(temp[0], temp[1]);
+      }
+    } catch(IOException e) {
+      System.err.println("IOException");
+    } finally {
+      try{
+      if(in != null) in.close();
+      } catch(IOException e) {};
+    }
+  
+  }
+
 }
